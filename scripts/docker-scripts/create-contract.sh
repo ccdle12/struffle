bf15154fdf38e517e896c305db41b3f6670a4bd0
ccdle12 <chris.coverdale24@gmail.com>
2018-04-26 22:03:50 +0100

    Updating constructor

--- scripts/docker-scripts/create-contract.sh
+++ scripts/docker-scripts/create-contract.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CONTRACTNAME=$1
+
 cd ./code/contracts
 
 cat <<EOF > ${CONTRACTNAME}.sol
@@ -8,7 +9,7 @@ pragma solidity ^0.4.18;
 
 contract ${CONTRACTNAME} {
 
-  function ${CONTRACTNAME}() {
+  constructor() {
     // constructor
   }
 }
